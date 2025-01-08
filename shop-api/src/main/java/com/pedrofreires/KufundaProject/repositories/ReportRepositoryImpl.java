0a44b61700b34c3472951ddfd46810fe8e1e013e
PedroH183 <pedro_freires18@outlook.com>
2025-01-07 21:50:17 -0300

    feat: implemented getReportByDate Query in Database

--- shop-api/src/main/java/com/pedrofreires/KufundaProject/repositories/ReportRepositoryImpl.java
+++ shop-api/src/main/java/com/pedrofreires/KufundaProject/repositories/ReportRepositoryImpl.java
@@ -1,20 +1,22 @@
 package com.pedrofreires.KufundaProject.repositories;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
+
 import com.pedrofreires.KufundaProject.domain.shop.Shop;
-import com.pedrofreires.KufundaProject.domain.shop.ShopReportDTO;
 import com.pedrofreires.KufundaProject.repositories.ShopReport.IReportRepository;
 
 
+// A diferença entre o entityManager.createNativeQuery e entity.CreateQuery
+// é que este ultimo opera sobre entidades do sistema, o outro opera diretamente no banco
+
+@Repository
 public class ReportRepositoryImpl implements IReportRepository {
 
     @PersistenceContext
@@ -22,7 +24,6 @@ public class ReportRepositoryImpl implements IReportRepository {
 
     @Override
     public List<Shop> getShopByFilters(Date dataInicio, Date dataFim, Float valorMinimo) {
-        // TODO : MELHORAR ESSE CODIGO !! 
 
         StringBuilder s = new StringBuilder();
 
@@ -39,35 +40,15 @@ public class ReportRepositoryImpl implements IReportRepository {
         Query query = entityManager.createQuery(s.toString());
 
         query.setParameter("dataInicio", dataInicio);
-        if (dataFim != null) {
-            query.setParameter("dataFim", dataFim); 
-        }
-        if (valorMinimo != null) {
-            query.setParameter("valorMinimo", valorMinimo);
-        }
+        this.setParameter(query, "dataFim", dataFim);
+        this.setParameter(query, "valorMinimo", valorMinimo);
+
         return query.getResultList();
     }
 
-    @Override
-    public ShopReportDTO getReportByDate(Date dataInicio, Date dataFim) {
-        StringBuilder s = new StringBuilder();
-
-        s.append("select count(s.id), sum(s.total), avg(s.total) " );
-        s.append(" from shopping.shop s" );
-        s.append(" where s.date >= :dataInicio ");
-        s.append(" and s.date <= :dataFim");
-
-        Query query = entityManager.createNativeQuery(s.toString());
-        query.setParameter("dataInicio", dataInicio);
-        query.setParameter("dataFim", dataFim);
-
-        Object[] ob = (Object[]) query.getSingleResult();
-
-        ShopReportDTO shopReportDTO = new ShopReportDTO();
-        shopReportDTO.setMean((Double) ob[2]);
-        shopReportDTO.setTotal((Double) ob[1]);
-        shopReportDTO.setCount(((BigInteger) ob[0]).intValue());
-
-        return shopReportDTO;
+    public <T> void setParameter(Query query, String field, T value){
+        if (value != null) {
+            query.setParameter(field, value);
+        }
     }
 }
